f80ade8c93260966767594e7169bb7386b3181b4
Tamara <tkharms@alaska.edu>
2022-01-10 22:22:33 -0900

    Lab1 data

---
+++ R_Scripts/01_Lab1.R
@@ -0,0 +1,11 @@
+### Lab 1: Intro to R ###
+
+## Inputs:
+    # Lab1data.csv: assignment data
+    # Length and mass measurements of 2 species of small mammals
+
+## Outputs:
+    # 
+
+library(here)
+library(tidyverse)
