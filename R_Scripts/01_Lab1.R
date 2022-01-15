a685ba3211327f96620ace2f8f6c405c159039e3
Tamara <tkharms@alaska.edu>
2022-01-14 16:20:41 -0900

    Lab 1 in-class script updates

--- R_Scripts/01_Lab1.R
+++ R_Scripts/01_Lab1.R
@@ -1,11 +1,22 @@
 ### Lab 1: Intro to R ###
+## Intro to data import/export
+## Basic plotting functions
 
 ## Inputs:
     # Lab1data.csv: assignment data
-    # Length and mass measurements of 2 species of small mammals
+      # Length and mass measurements of 2 species of small mammals
 
 ## Outputs:
-    # 
+    # Summary statistics table
+    # Box plot
 
 library(here)
 library(tidyverse)
+
+dat <- read.csv(here("data", "Lab1data.csv"))
+
+plot(dat$length_mm, dat$mass_g)
+
+ggplot(dat, aes(x= length_mm, y = mass_g)) +
+  geom_point(color = "blue") +
+  ylab("body mass (g)")
