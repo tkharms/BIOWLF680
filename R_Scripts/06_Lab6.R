a22d75c7e0d095b4afae88a4790b7579aaa47506
Tamara <tkharms@alaska.edu>
2022-02-09 13:08:20 -0900

    Lab 06

--- R_Scripts/06_Lab6.R
+++ R_Scripts/06_Lab6.R
@@ -2,6 +2,7 @@
 
 library(here)
 library(tidyverse)
+library(pwr)
 
 ### Power and t-tests ###
 
@@ -33,10 +34,34 @@ power.t.test(n=25, sd = sdt, sig.level = 0.05, power = 0.8, type = "two.sample",
 power.t.test(n = 25, delta = 8.345515, sd = sdt, sig.level = 0.05, type = "paired", alternative = "two.sided")
 
 ### Power and ANOVA ###
-# Data from Lecture 5
-dat.aov <- read.csv(here("data", "Lect07_data.csv"), header = TRUE)
-
 ## Effect size in ANOVA power calculation:
 # d = difference between means / pooled SD
 # f = 0.5*d (for two groups)
 # partial eta-squared = SS-treatment / (SS-treatment + SS-residuals)
+
+# Data from Lecture 5
+dat.aov <- read.csv(here("data", "Lect07_data.csv"), header = TRUE)
+
+mod.aov <- aov(NPP ~ treat, data = dat.aov)
+
+# Power of performed test
+power.anova.test(groups = 3, n = 10, between.var = 539, within.var = 675, sig.level = 0.05)
+
+## More flexibility in using hypothesized effect sizes with the pwr library
+# Sample size needed to detect a significant difference
+sumdat <- dat.aov %>% group_by(treat) %>%
+            summarize(across(NPP, list(mn = mean, stdev = sd)))
+
+# Difference between two most different groups
+d <- sumdat[2,2] - sumdat[3,2]
+sdpool <- sqrt((sumdat[2,3]^2 + sumdat[3,3]^2)/2)
+dc <- as.numeric(d/sdpool)
+
+pwr.anova.test(k = 3, f = 0.5*dc, sig.level = 0.05, power = 0.8)
+
+# Minimum detectable difference
+pwr.anova.test(k = 3, n = 5, sig.level = 0.05, power = 0.8)
+
+# Sample size required for significant difference with medium average effect size between all groups
+f <- 0.5
+pwr.anova.test(k = 3, f = 0.5, sig.level = 0.05, power = 0.8)
