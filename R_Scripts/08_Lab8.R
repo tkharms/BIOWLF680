2b6cfd23be9dd0e4f2281aa93b173fef0e184908
Tamara <tkharms@alaska.edu>
2022-03-14 23:06:58 -0800

    Lab 8

---
+++ R_Scripts/08_Lab8.R
@@ -0,0 +1,93 @@
+### Lab 8: Mixed effects models & multimodel inference ###
+
+library(nlme)
+library(lme4)
+library(here)
+library(tidyverse)
+library(AICcmodavg)
+library(MuMIn)
+
+### Data from Zuur ###
+RIKZ <- read.delim(here("data", "Lect14_data.txt"), head = TRUE)
+
+#plot data with grouping structure
+NAP.pl <- RIKZ %>% ggplot(aes(x = NAP, y = Richness)) +
+                   geom_point() +
+                   facet_wrap(~as.factor(Beach), nrow = 2)
+
+#convert Beach to a factor
+RIKZ$fBeach <- as.factor(RIKZ$Beach)
+
+### Fit multilevel models ###
+## Group-level intercept ##
+# Using lme4
+randint <- lmer(Richness ~ NAP + (1|fBeach), data = RIKZ)
+
+# Check assumptions
+plot(randint)
+# This looks scary
+
+qqnorm(resid(randint))
+qqline(resid(randint))
+# long tail
+
+summary(randint)
+
+# Check assumptions on random effects
+qqnorm(ranef(randint)$fBeach[[1]])
+plot(ranef(randint)$fBeach[[1]])
+#not great
+
+## plot residuals by  group; allows visualization of variance with covariate and groups simultaneously
+coplot(resid(randint) ~ NAP|fBeach, data=RIKZ)
+
+## Correlated group-level slopes and intercepts ##
+randintsl <- lmer(Richness ~ NAP + (1 + NAP|fBeach), data = RIKZ)
+# Issues with convergence here. Let's try another optimizer
+
+randintsl <- lmer(Richness ~ NAP + (1 + NAP|fBeach), data = RIKZ, 
+                  control=lmerControl(optimizer="bobyqa"))
+
+# Check assumptions
+plot(randintsl)
+# This looks scary
+
+qqnorm(resid(randintsl))
+qqline(resid(randintsl))
+# long tails
+
+summary(randintsl)
+
+# Check assumptions on random effects
+qqnorm(ranef(randintsl)$fBeach[[1]])
+plot(ranef(randintsl)$fBeach[[1]])
+#not great
+
+## Uncorrelated group-level slopes and intercepts ##
+rand.uncorr <- lmer(Richness ~ NAP + (1|fBeach) + (0 + NAP|fBeach), data = RIKZ)
+
+# Check assumptions
+plot(rand.uncorr)
+# This looks scary
+
+qqnorm(resid(rand.uncorr))
+qqline(resid(rand.uncorr))
+# long tails
+
+summary(rand.uncorr)
+
+# Check assumptions on random effects
+qqnorm(ranef(rand.uncorr)$fBeach[[1]])
+plot(ranef(rand.uncorr)$fBeach[[1]])
+#not great
+       
+### Compare models ### 
+# AIC table from AICcmodavg
+aictab(list(randint = randint, randintsl = randintsl, rand.uncorr = rand.uncorr))
+# Group-level intercept & correlated slopes and intercepts are equivalent. Let's work with the more parsimonious model (group-level intercepts)
+
+### Estimate a pseudo-R2 for the full model and for the fixed effects only ###
+# From MuMIN package
+#R2m: marginal R2, variance explained by fixed factors only
+#R2c: conditional R2, variance explained by fixed+random factors
+r.squaredGLMM(randint)
